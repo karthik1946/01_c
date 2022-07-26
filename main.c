b2954be854f532e3067f6d5f662ef61803125450
Deshik18 <110047070+Deshik18@users.noreply.github.com>
2022-07-26 22:48:46 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -1,52 +1,29 @@
-/*
 #include <stdio.h>
-#include <string.h>
 
-struct vector{
-    int x;
-    int y;
-};
+typedef struct complex_no{
+    int real;
+    int imag;
+}comp;
 
-int main()
-{
-    struct vector v1,v2;
-    v1.x=10;
-    v1.y=24;
-
-    v2.x=15;
-    v2.y=20;
-
-    printf("X coordinate is %d, Y coordinate is %d",v1.x,v1.y);
-    return 0;
-}
-*/
-#include <stdio.h>
-#include <string.h>
-
-struct vector{
-    int x;
-    int y;
-};
-
-struct vector sumVector(struct vector v1,struct vector v2){
-    struct vector v3;
-    v3.x = v1.x + v2.x;
-    v3.y = v1.y + v2.y;
-    return v3;
+void display(comp c){
+    printf("The complex number is %d + i(%d)\n",c.real,c.imag);
 }
 
 
 int main()
 {
-    struct vector v1,v2,sum;
-    v1.x=10;
-    v1.y=24;
-
-    v2.x=15;
-    v2.y=20;
-    sum=sumVector(v1,v2);
-
-    printf("Sum of x dim of vectors is %d\nSum of y dim of vectors is %d",sum.x,sum.y);
+    comp c[5];
+    int j=0;
+    for(int i=1; i<=5; i++){
+        printf("Enter the real part of complex number %d: ",i);
+        scanf("%d",&c[j].real);
+        printf("Enter the imaginary part of complex number %d: ",i);
+        scanf("%d",&c[j].imag);
+        j++;
+    }
+    for(int i=0; i<5; i++){
+        display(c[i]);
+    }
 
     return 0;
 }
